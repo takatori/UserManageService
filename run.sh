fc8abb58088c5e567823c90e45eb82be3d2ae091
Satoshi Takatori <takatori@ws.cs.kobe-u.ac.jp>
2015-07-16 13:49:58 +0900

    modify forever path

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 mode="dev"
 env="dev"
-start_prod="node_modules/.bin/forever start --uid 'usermanage' -a app.js"
-start_dev="node_modules/.bin/nodemon app.js"
-start_test="node_modules/.bin/mocha --reporter spec"
+start_prod="./node_modules/.bin/forever start --uid 'usermanage' -a app.js"
+start_dev="./node_modules/.bin/nodemon app.js"
+start_test="./node_modules/.bin/mocha --reporter spec"
 
 while getopts m:e: OPT
 do
